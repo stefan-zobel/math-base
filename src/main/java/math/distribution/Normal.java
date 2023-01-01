4de355c94b3a3d8d30e2c305647e7bca3a1b53ef
stefan-zobel <stefan-zobel@users.noreply.github.com>
2023-01-01 21:37:43 +0000

    add Normal

---
+++ src/main/java/math/distribution/Normal.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2013 Stefan Zobel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package math.distribution;
+
+import math.cern.ProbabilityFuncs;
+import static math.MathConsts.SQRT_TWO_PI;
+
+/**
+ * Normal (a.k.a Gaussian) distribution.
+ */
+public class Normal implements ContinuousDistribution {
+
+    /** Mean of this distribution */
+    private final double mean;
+
+    /** Variance of this distribution */
+    private final double variance;
+
+    /** 1.0 / (stdDev * sqrt(2 * PI)) */
+    private final double factor;
+
+    public Normal() {
+        this(0.0, 1.0);
+    }
+
+    public Normal(double mean, double stdDev) {
+        if (stdDev <= 0.0) {
+            throw new IllegalArgumentException("Standard deviation must be positive (" + stdDev + ")");
+        }
+        this.mean = mean;
+        this.variance = stdDev * stdDev;
+        this.factor = (1.0 / (this.variance * SQRT_TWO_PI));
+    }
+
+    @Override
+    public double pdf(double x) {
+        double xMinusMu = (x - mean);
+        return factor * Math.exp(-(xMinusMu * xMinusMu) / (2.0 * variance));
+    }
+
+    @Override
+    public double cdf(double x) {
+        return ProbabilityFuncs.normal(mean, variance, x);
+    }
+
+    @Override
+    public double inverseCdf(double probability) {
+        if (probability <= 0.0) {
+            return Double.NEGATIVE_INFINITY;
+        }
+        if (probability >= 1.0) {
+            return Double.POSITIVE_INFINITY;
+        }
+        return findRoot(probability, mean(), -Double.MAX_VALUE, Double.MAX_VALUE);
+    }
+
+    @Override
+    public double mean() {
+        return mean;
+    }
+
+    @Override
+    public double variance() {
+        return variance;
+    }
+}
